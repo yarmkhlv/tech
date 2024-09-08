cfb15bc83c490d8a66d394f8904fb72cdd272364
Yaroslav <yarmkhlv@gmail.com>
2024-09-09 01:28:44 +0300

    started working AdDetailsPage and form

--- src/pages/AdDetailsPage.tsx
+++ src/pages/AdDetailsPage.tsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from 'react';
+import clsx from 'clsx';
 import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import { Loader } from '../components/Loader';
 
+import { formatCurrency } from '../components/Card/helpers/formatCurrency';
+
 import { Advertisment } from '../components/Card/types';
+
+import styles from './adDetailsPage.module.scss';
+import { CrossIcon } from './Cross';
 
 export function AdDetailsPage() {
     const { id } = useParams<{ id: string }>();
@@ -12,6 +19,15 @@ export function AdDetailsPage() {
     );
     const [loading, setLoading] = useState(true);
     const [errorAfterFetching, setErrorAfterFetching] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        resetField,
+        formState: { errors },
+    } = useForm<Partial<Advertisment>>();
 
     useEffect(() => {
         const fetchAdvertisement = async () => {
@@ -23,6 +39,7 @@ export function AdDetailsPage() {
                     if (response.ok) {
                         const data = await response.json();
                         setAdvertisement(data);
+                        reset(data);
                     } else {
                         console.error(
                             'Failed to fetch advertisement:',
@@ -40,7 +57,36 @@ export function AdDetailsPage() {
         };
 
         fetchAdvertisement();
-    }, [id]);
+    }, [id, reset]);
+
+    const onSubmit = async (data: Partial<Advertisment>) => {
+        if (id) {
+            try {
+                const response = await fetch(
+                    `http://localhost:3000/advertisements/${id}`,
+                    {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(data),
+                    },
+                );
+                if (response.ok) {
+                    const updatedData = await response.json();
+                    setAdvertisement(updatedData);
+                    setIsEditing(false);
+                } else {
+                    console.error(
+                        'Failed to update advertisement:',
+                        response.statusText,
+                    );
+                }
+            } catch (error) {
+                console.error('Error updating advertisement:', error);
+            }
+        }
+    };
 
     if (errorAfterFetching)
         return (
@@ -53,14 +99,116 @@ export function AdDetailsPage() {
 
     return (
         <div>
-            <img src={advertisement.imageUrl} alt={advertisement.name} />
-            <h1>{advertisement.name}</h1>
-            {advertisement.description && (
-                <p>Описание: {advertisement.description}</p>
+            {isEditing ? (
+                <div className={styles.formContainer}>
+                    <h1 className={styles.title}>Редактирование объявления</h1>
+                    <form
+                        className={styles.form}
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <div className={styles.inputGroup}>
+                            <label className={styles.label} htmlFor="name">
+                                Название объявления
+                            </label>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                id="name"
+                                {...register('name', { required: true })}
+                            />
+                            <button
+                                type="button"
+                                className={styles.clearBtn}
+                                onClick={() => resetField('name')}
+                            >
+                                <CrossIcon />
+                            </button>
+
+                            {errors.name && <span>Name is required</span>}
+                        </div>
+                        <div className={styles.inputGroup}>
+                            <label
+                                className={styles.label}
+                                htmlFor="description"
+                            >
+                                Описание объявления
+                            </label>
+                            <textarea
+                                className={clsx(styles.input, styles.textArea)}
+                                id="description"
+                                {...register('description')}
+                            />
+                            <button
+                                type="button"
+                                className={styles.clearBtn}
+                                onClick={() => resetField('description')}
+                            >
+                                <CrossIcon />
+                            </button>
+                        </div>
+                        <div className={styles.inputGroup}>
+                            <label className={styles.label} htmlFor="imageUrl">
+                                Картинка
+                            </label>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                id="imageUrl"
+                                {...register('imageUrl', { required: true })}
+                            />
+                            <button
+                                type="button"
+                                className={styles.clearBtn}
+                                onClick={() => resetField('imageUrl')}
+                            >
+                                <CrossIcon />
+                            </button>
+                            {errors.imageUrl && (
+                                <span>Image URL is required</span>
+                            )}
+                        </div>
+                        <div className={styles.inputGroup}>
+                            <label className={styles.label} htmlFor="price">
+                                Цена
+                            </label>
+                            <input
+                                className={styles.input}
+                                type="number"
+                                min={1}
+                                id="price"
+                                {...register('price', { required: true })}
+                            />
+                            <button
+                                type="button"
+                                className={styles.clearBtn}
+                                onClick={() => resetField('price')}
+                            >
+                                <CrossIcon />
+                            </button>
+                            {errors.price && <span>Price is required</span>}
+                        </div>
+
+                        <button className={styles.submitButton} type="submit">
+                            Save
+                        </button>
+                    </form>
+                </div>
+            ) : (
+                <>
+                    <img
+                        src={advertisement.imageUrl}
+                        alt={advertisement.name}
+                    />
+                    <h1>{advertisement.name}</h1>
+                    {advertisement.description && (
+                        <p>Описание: {advertisement.description}</p>
+                    )}
+                    <p>Price: {formatCurrency(advertisement.price)}</p>
+                    <p>Views: {advertisement.views}</p>
+                    <p>Likes: {advertisement.likes}</p>
+                    <button onClick={() => setIsEditing(true)}>Edit</button>
+                </>
             )}
-            <p>Price: {advertisement.price}</p>
-            <p>Views: {advertisement.views}</p>
-            <p>Likes: {advertisement.likes}</p>
         </div>
     );
 }
---
+++ src/pages/Cross.tsx
@@ -0,0 +1,18 @@
+export function CrossIcon() {
+    return (
+        <svg
+            width="100%"
+            height="100%"
+            data-icon="close"
+            name="close"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M11.58.42a.83.83 0 0 0-1.18 0L6 4.82 1.57.43A.83.83 0 1 0 .4 1.59l4.41 4.42-4.4 4.4a.83.83 0 1 0 1.17 1.18L6 7.2l4.41 4.4a.83.83 0 0 0 1.18-1.17L7.18 6l4.4-4.42a.83.83 0 0 0 0-1.17Z"
+                fill="currentColor"
+            ></path>
+        </svg>
+    );
+}
